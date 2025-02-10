baeb0e285a713bd9a2e1c9399780271b083bcb75
PRINCE BHARATBHAI BHIKADIYA <87688956+Prince1Bhikadiya@users.noreply.github.com>
2025-02-10 17:28:14 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -14,7 +14,7 @@ const sora = Sora({
 import Nav from '../components/Nav';
 import Header from '../components/Header';
 import TopLeftImg from '../components/TopLeftImg';
-import PrivacyPolicy from './PrivacyPolicy';
+
 
 const Layout = ({children}) => {
   return (
